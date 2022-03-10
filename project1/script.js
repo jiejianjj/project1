72f02e919528c52831cbfa97bf5b4cc46e86b045
Jie Jian (JJ) <61826920+pearly0226@users.noreply.github.com>
2022-03-09 23:49:25 -0500

    new

--- project1/script.js
+++ project1/script.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
-  $(document).on("tap", function () {
-    $(".sung").css({ "display": "block" });
-  });
+const sung = document.getElementById("sung");
+
+document.body.addEventListener("touchstart", (e) => {
+  sung.setAttribute("style", "display: block;");
+});
+document.body.addEventListener("touchend", (e) => {
+  sung.setAttribute("style", "display: none;");
 });
