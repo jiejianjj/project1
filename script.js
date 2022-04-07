58daf50aec3fff9b26c6e5812fd1fc6a1406f56d
Jie Jian (JJ) <61826920+pearly0226@users.noreply.github.com>
2022-04-07 17:02:38 -0400

    variable

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
     $("#darkmode").click(function(){ $('body').toggleClass('darkmode');});
+    $(document).ready(function(){
+
+        console.log('script loaded');
+    
+        const wghtmax = 900;
+    
+        //standard sliders
+        $('.axis-range').on('input', function(){
+    
+            let value = parseInt($(this).val()); //get slider input value
+            // console.log(value);
+    
+            const slidertype = $(this).data('type'); //get slider axis
+    
+            // console.log("--"+slidertype, value );
+    
+            $('#bigletter').css("--"+slidertype, value);
+    
+        });
+        //mouseposition
+        $('#intro').mousemove(function(event) {
+    
+            console.log(event.pageX, event.pageY);
+    
+            let cursorX = event.pageX / $(this).width();
+    
+            let settingX = Math.floor(cursorX * wghtmax);
+    
+            // console.log( settingX, settingY)
+    
+            $("#bigletter").css({
+                "--wght": settingX
+            });
+            
+        });
+    });
+    
+    // Function to generate random number 
+    function randomNumber(min, max) { 
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
+    
+    
 });
