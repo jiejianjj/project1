5d6e52d6defbd42aba132653069ce480dfc6555c
Jie Jian (JJ) <61826920+pearly0226@users.noreply.github.com>
2022-04-13 21:31:59 -0400

    vf

--- script.js
+++ script.js
@@ -35,13 +35,54 @@ $(document).ready(function(){
             });
             
         });
-    });
+        //click buttons
+	$('#pride').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 100,
+		});	
+	});
+    $('#greed').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 200,
+		});	
+	});
+    $('#lust').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 300,
+		});	
+	});
+    $('#envy').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 400,
+		});	
+	});
+    $('#gluttony').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 500,
+		});	
+	});
+    $('#wrath').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 600,
+		});	
+	});
+    $('#sloth').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 700,
+		});	
+	});
+    $('#sins').click(function(event) {
+		$("#bigletter").css({
+			"--wght": 800,
+		});	
+	});
+
+
+    }
+    );
     
     // Function to generate random number 
     function randomNumber(min, max) { 
         return Math.floor(Math.random() * (max - min) + min);
     }
-    
-    
-    
 });
