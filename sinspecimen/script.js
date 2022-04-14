888e78c810d4f54a130a2fc0d9e96cd12686e7c9
Jie Jian (JJ) <61826920+pearly0226@users.noreply.github.com>
2022-04-14 12:55:03 -0400

    new

--- sinspecimen/script.js
+++ sinspecimen/script.js
@@ -5,19 +5,6 @@ $(document).ready(function(){
     
         const wghtmax = 900;
     
-        //standard sliders
-        $('.axis-range').on('input', function(){
-    
-            let value = parseInt($(this).val()); //get slider input value
-            // console.log(value);
-    
-            const slidertype = $(this).data('type'); //get slider axis
-    
-            // console.log("--"+slidertype, value );
-    
-            $('#bigletter').css("--"+slidertype, value);
-    
-        });
         //mouseposition
         $('#intro').mousemove(function(event) {
     
